50c19d21ec3b5a6877badfa5bb7f2532b1506cb8
burkhardtdr <burkhardtdr@gmail.com>
2022-03-10 08:26:55 -0800

    New file created.

---
+++ test.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  Test file
+ *
+ *        Version:  1.0
+ *        Created:  03/10/2022 08:22:01 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+    return 0;
+}
+
+
